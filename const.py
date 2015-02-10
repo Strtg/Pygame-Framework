4504dd18be73eb0412fa25f335afa2e64e934600
Strtg <raughsuri@yahoo.com>
2015-02-10 12:51:07 +0100

    Framework of framework is under construction.
    Really nothing interesting yet.
    
    Cheers!

--- const.py
+++ const.py
@@ -3,10 +3,10 @@ The module with constants.
 
 '''
 # directories
-OUT_DIR = '' # the directory with front-end game data like graphics and sounds that can be safely modify
-IN_DIR = '' # the directory with back-end game data like monster or item statistics that can not be modded but required a new game to work
-VANILLA_DIR = '' # the directory with unmodified game
-CFG_DIR = '' # the directory with configuration files
-SAVE_DIR = '' # the directory with saved game files
-IMG_DIR = ''
-SND_DIR = ''
+OUT_DIR = '' # the directory for front-end game data like graphics and sounds that can be safely modify
+IN_DIR = '' # the directory for back-end game data like monster or item statistics that can not be modded but required a new game to work
+VANILLA_DIR = '' # the directory for unmodified game
+CFG_DIR = '' # the directory wfor configuration files
+SAVE_DIR = '' # the directory for saved game files
+IMG_DIR = '' # the directory for images for sprites
+SND_DIR = '' # the directory for sound
